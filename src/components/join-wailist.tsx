3cce47b5d5269670b38c504c5c7c61f78fe92a2f
Jona <llamasjonat@gmail.com>
2024-10-25 19:37:19 -0300

    feat: added JoinWailist component

---
+++ src/components/join-wailist.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useState } from 'react';
+import { DoorOpen } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+
+export function JoinWaitlist(props: {
+  label: string;
+  onSubmit: (pubkey: string) => any;
+  loading: boolean;
+}) {
+  const { label, onSubmit, loading } = props;
+
+  const [pubkey, setPubkey] = useState<string | null>(null);
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>{label}</Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-sm h-auto">
+        <DialogHeader className="flex flex-col">
+          <div className="flex justify-center items-center w-12 h-12 mb-2 bg-card rounded-full">
+            <DoorOpen />
+          </div>
+          <DialogTitle className={'text-lg font-bold'}>
+            Welcome to Eventro
+          </DialogTitle>
+          <DialogDescription className="text-white">
+            Please log in to register for a beta.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="flex flex-col">
+          <div className="flex flex-col w-full gap-4">
+            <div className="flex flex-col gap-2 w-full">
+              <Label htmlFor="title">Nostr</Label>
+              <Input
+                placeholder="NIP-05 or pubkey"
+                type="text"
+                value={pubkey as string}
+                onChange={(e) => setPubkey(e.target.value)}
+              />
+            </div>
+            <Button
+              className="w-full"
+              size="lg"
+              disabled={!pubkey || loading}
+              onClick={() => onSubmit(pubkey as string)}
+            >
+              {loading ? 'Loading...' : 'Sign me up'}
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
