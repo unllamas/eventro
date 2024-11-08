c9d63ad111a6fda7c97036db9d0158b41a1965cb
Jona <llamasjonat@gmail.com>
2024-11-07 22:21:39 -0300

    fix: general changes

--- src/app/check-out/[id]/page.tsx
+++ src/app/check-out/[id]/page.tsx
@@ -5,24 +5,26 @@ import { prisma } from '@/services/prismaClient';
 export default async function Page(props: { params: { id: string } }) {
   const id = props.params.id;
 
-  const event = await prisma.event.findUnique({
-    where: {
-      nostrId: id as string,
-    },
-  });
+  const { event, ticket, sales } = await prisma.$transaction(async () => {
+    const event = await prisma.event.findUnique({
+      where: {
+        nostrId: id as string,
+      },
+    });
 
-  if (!event) return <div>Event not found</div>;
+    const ticket = await prisma.ticket.findFirst({
+      where: {
+        eventId: event?.id as string,
+      },
+    });
 
-  const ticket = await prisma.ticket.findFirst({
-    where: {
-      eventId: event?.id as string,
-    },
-  });
+    const sales = await prisma.ticketSale.count({
+      where: {
+        eventId: event?.id as string,
+      },
+    });
 
-  const sales = await prisma.ticketSale.count({
-    where: {
-      eventId: event?.id as string,
-    },
+    return { event, ticket, sales };
   });
 
   return <CheckOut event={event} ticket={ticket} ticketsSales={sales} />;
